3eba1e1c8ccb4f6cd2076d36c70a7fb383f810f6
Aayan wakil <aayanwakil@Biztechs-MacBook-Pro-2.local>
2019-07-25 14:22:59 +1000

    my 2nd commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Smart by Syed Wakil';
+  summary = " This is a smart Application and covers most of the Angular 8 topics"
+  
+}
